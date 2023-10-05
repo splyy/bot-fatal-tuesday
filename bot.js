978212431e342c37bf70358da631fdfff9a6c291
Splyy <simplyy.design@gmail.com>
2023-10-05 21:16:00 +0200

    failed env key

--- bot.js
+++ bot.js
@@ -7,15 +7,15 @@ const client = new TwitterApi({
     appKey: process.env.API_KEY, 
     appSecret: process.env.API_SECRET, 
     accessToken: process.env.ACCESS_TOKEN, 
-    accessSecret: process.env.ACCESS_SECRET 
+    accessSecret: process.env.ACCESS_TOKEN_SECRET 
 });
 
 // First, upload your media on Twitter
-// const mediaId = await client.v1.uploadMedia('./video.mp4');
-// console.log(mediaId);
+const mediaId = await client.v1.uploadMedia('./video.mp4');
 
-// Then post
-// mediaId is a string[], can be given to .tweet
-await client.v2.tweet('toto');
+// Then post, mediaId is a string[], can be given to .tweet
+await client.v2.tweet({
+    media: { media_ids: [mediaId] }
+});
 
 // finally `npm run bot` to execute
